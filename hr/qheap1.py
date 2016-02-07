9e7aee1f95600424c5a2431200b6ba205288a107
Sergey Belikov <belikov.sergey@gmail.com>
2016-02-07 22:38:22 +0100

    qheap1 solution

--- hr/qheap1.py
+++ hr/qheap1.py
@@ -1,22 +1,69 @@
 class MinHeap(object):
 
-    def __init__(self, items):
-        self.items = None
-        for item in items:
-            self.insert(item)
-
-    def count(self):
-        return len(self.items)-1
+    def __init__(self):
+        self.items = [None]
+        self.count = 0
 
     def insert(self, item):
         self.items.append(item)
-        self._sink()
+        self.count += 1
+        self._swim()
 
-    def delMax(self):
-        pass
+    def get_min(self):
+        if self.count == 0:
+            return None
+        return self.items[1]
+
+    def delete(self, element):
+        index = 1
+        while index < self.count and self.items[index] != element:
+            index += 1
+
+        if index == self.count:
+            self.items.pop()
+            self.count -= 1
+        else:
+            self.count -= 1
+            self.items[index] = self.items.pop()
+            self._sink(index)
+
 
     def _sink(self, index=None):
         index = 1 if index is None else index
+        while 2*index <= self.count:
+            if 2*index+1 <= self.count and self.items[2*index+1] < self.items[2*index]:
+                k = 2*index+1
+            else:
+                k = 2*index
+            if self.items[index] < self.items[k]:
+                break
+            self._swap(index, k)
+            index = k
 
     def _swim(self, index=None):
-        index = self.count() if index is None else index
+        index = self.count if index is None else index
+        while index > 1 and self.items[index / 2] > self.items[index]:
+            self._swap(index, index / 2)
+            index /= 2
+
+    def _swap(self, i, j):
+        t = self.items[i]
+        self.items[i] = self.items[j]
+        self.items[j] = t
+
+
+def main():
+    number_of_queries = int(raw_input())
+    min_heap = MinHeap()
+    while number_of_queries > 0:
+        inp = raw_input().split()
+        if inp[0] == "1":
+            min_heap.insert(int(inp[1]))
+        elif inp[0] == "2":
+            min_heap.delete(int(inp[1]))
+        elif inp[0] == "3":
+            print(min_heap.get_min())
+        number_of_queries -= 1
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+from hr.qheap1 import MinHeap
+
+
 def main():
-    from bigint import BigInt
-    a = BigInt("123")
-    b = BigInt("-959")
-    c = a.multiply(b)
-    print(c)
+    h = MinHeap()
+    h.insert(286789035)
+    h.insert(255653921)
+    h.insert(274310529)
+    h.insert(494521015)
+    print(h.get_min())
+    h.delete(255653921)
+    h.delete(286789035)
+    print(h.get_min())
+    h.insert(236295092)
+    h.insert(254828111)
+    h.delete(254828111)
+    h.insert(465995753)
+    h.insert(85886315)
+    h.insert(7959587)
+    h.insert(20842598)
+    h.delete(7959587)
+    print(h.get_min())
+    h.insert(-51159108)
+    print(h.get_min())
+    h.delete(-51159108)
+    print(h.get_min())
+    h.insert(789534713)
     print("Cya!")
 
 
