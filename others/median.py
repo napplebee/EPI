a1072780119ffe5bb7f78b71192015142d29ea2d
Sergey Belikov <sergey@snappcar.nl>
2016-03-01 11:45:44 +0100

    median problem scratch

--- others/median.py
+++ others/median.py
@@ -1,20 +1,30 @@
 import random as rnd
 
+
+class Heap(object):
+    pass
+
+
 class NumberStorageNde(object):
     storage = []
 
     def __init__(self, size, index=0):
         self.index = index
         self.size = size
-        for _ in range(0, size):
-            self.storage.append(rnd.randint(0, size*2))
+        # just want to have number in order
+        seed = rnd.randint(0, size*2)
+        for i in range(0, size):
+            self.storage.append(seed+i)
 
     def next(self):
         element = self.storage[self.index]
         self.index += 1
-        return element
+        return element, self.index
 
 
 
 def median():
+    storage_nodes = []
+    number_of_nodes = 10
+    for i in range()
     heap = Heap()
