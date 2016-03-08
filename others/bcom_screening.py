8bd69cb81f14ba2d1ddf8b38a1da8b77de3cc719
Sergey Belikov <sergey@snappcar.nl>
2016-03-08 17:38:15 +0100

    using hashset

--- others/bcom_screening.py
+++ others/bcom_screening.py
@@ -1,6 +1,6 @@
 class Problem4(object):
     N = None
-    items = []
+    items = None
     result = 0 # default output
     """
     Sum of 2 numbers in an array
@@ -15,15 +15,12 @@ class Problem4(object):
     Output 1 if there exists a pair of numbers in the array such that their sum equals N. If such a pair does not exist, output 0.
     """
 
-    def read_input(self):
+    def naive_input(self):
         self.N = int(raw_input("N:"))
         M = int(raw_input("M:"))
+        self.items = []
         for _ in range(0, M):
             self.items.append(int(raw_input()))
-
-    def output(self):
-        self.naive_impl()
-        print self.result
 
     def naive_impl(self):
         i = 0
@@ -38,8 +35,36 @@ class Problem4(object):
                 k += 1
             i += 1
 
+    def store_in_hash(self):
+        self.items = {}
+        self.N = int(raw_input("N:"))
+        M = int(raw_input("M:"))
+        for _ in range(0, M):
+            item = int(raw_input())
+            if item in self.items:
+                self.items[item] += 1
+            else:
+                self.items[item] = 1
+
+    def hash_impl(self):
+        for k in self.items.keys():
+            num = self.N - k
+            # handle special case
+            if num == k and self.items[k] != 1:
+                self.result = 1
+                return
+            if num in self.items:
+                self.result = 1
+                return
+
+    def output(self):
+        self.naive_impl()
+        print(self.result)
+
+
+
 
 def test():
     p = Problem4()
-    p.read_input()
+    p.naive_input()
     p.output()
