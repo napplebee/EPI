62c61c840ba84fab2d40e6aba6985362cf1ab267
Sergey Belikov <sergey@snappcar.nl>
2016-03-09 14:58:50 +0100

    problem2

---
+++ others/bcom_screening2.py
@@ -0,0 +1,37 @@
+class Problem2(object):
+    hashset = {}
+    """
+Problem II.
+======================================================================================
+
+Anagrams
+
+An anagram is a word that can be written as a permutation of the characters of another word, like "dirty room" and "dormitory" (ignore spaces).
+However, "the" and "thee" are not anagrams, since "the" only has a single "e" whereas "thee" has two "e" characters (spaces can occur zero, or multiple times, however).
+Given a list of strings of unknown length, one string per line, you should output groups of the provided words that are mutual anagrams.
+Output one group per line and separate the strings in each group by commas.
+Within a line, the expressions should be sorted lexicographically. If a group contains only a single element, output that one-element group on a single line. And the lines should also be output in lexicographical order.
+
+Given e.g. :
+pear
+amleth
+dormitory
+tinsel
+dirty room
+hamlet
+listen
+silent
+
+... the output would be:
+amleth,hamlet
+dirty room,dormitory
+listen,silent,tinsel
+pear
+    """
+
+    def input(self, strings):
+        for str in strings:
+            
+
+    def output(self):
+        pass
