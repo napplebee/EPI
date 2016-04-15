fc32f2936e75990698078538bf78fc0120c59368
napp <belikov.sergey@gmail.com>
2016-04-15 11:38:35 +0200

    Create hanoi_tower.py

---
+++ epi/hanoi_tower.py
@@ -0,0 +1,26 @@
+# 8.5
+
+
+def transfer(n, pegs, _from, _to, use):
+    print "transfer({}, {}, {}, {}, {})".format(n, pegs, _from, _to, use)
+    if n > 0:
+        transfer(n-1, pegs, _from, use, _to)
+        pegs[_to].append(pegs[_from][-1])
+        pegs[_from].pop()
+        print "Move from peg {0} to peg {1}".format(_from, _to)
+        # print pegs
+        # print
+        transfer(n-1, pegs, use, _to, _from)
+
+
+def move_tower_hanoi(n):
+    pegs = [list(reversed(range(0, n))), [], []]
+    print "Input:"
+    print pegs
+    transfer(n, pegs, 0, 1, 2)
+    print "Result:"
+    print pegs
+
+
+def test():
+    move_tower_hanoi(3)
