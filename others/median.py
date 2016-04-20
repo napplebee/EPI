07255db42c64d9da34663e0a980ed11f0956bd41
nope <nope@domain.com>
2016-04-20 21:55:13 +0200

    medians

--- others/median.py
+++ others/median.py
@@ -1,30 +1,16 @@
-import random as rnd
+
+class DistributedArray(object):
+
+    def __init__(self, servers_to_dispatch):
+        pass
+
+    def get(self, index):
+        pass
+
+    def size(self):
+        pass
 
 
-class Heap(object):
-    pass
-
-
-class NumberStorageNde(object):
-    storage = []
-
-    def __init__(self, size, index=0):
-        self.index = index
-        self.size = size
-        # just want to have number in order
-        seed = rnd.randint(0, size*2)
-        for i in range(0, size):
-            self.storage.append(seed+i)
-
-    def next(self):
-        element = self.storage[self.index]
-        self.index += 1
-        return element, self.index
-
-
-
-def median():
-    storage_nodes = []
-    number_of_nodes = 10
-    for i in range(0, 10):
-        heap = Heap()
+def find_median():
+    ar = DistributedArray()
+    median = get_order_stat(ar, ar.size() / 2)
